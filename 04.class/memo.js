e160e4d5d98196ba1cdb03f2bc144ccf1929df56
kazumi <kazumi121589@yahoo.co.jp>
2022-05-06 11:08:36 +0900

    Created NewMemo class.

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,12 +1,21 @@
 const fs = require('fs')
 process.stdin.setEncoding('utf8')
 
-const jsonMemo = fs.readFileSync('./memo_list.json', 'utf8')
-const stringMemo = JSON.parse(jsonMemo)
+class NewMemo {
+  readMemo () {
+    const jsonMemo = fs.readFileSync('./memo_list.json', 'utf8')
+    return JSON.parse(jsonMemo)
+  }
 
-process.stdin.on('data', input => {
-  const memoObject = { content: input }
-  stringMemo.push(memoObject)
-  const memo = JSON.stringify(stringMemo)
-  fs.writeFileSync('./memo_list.json', memo)
-})
+  createMemo () {
+    process.stdin.on('data', async input => {
+      const memoObject = { content: input }
+      const memos = await this.readMemo()
+      memos.push(memoObject)
+      const newMemos = JSON.stringify(memos)
+      fs.writeFileSync('./memo_list.json', newMemos)
+    })
+  }
+}
+
+new NewMemo().createMemo()
