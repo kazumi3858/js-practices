e4743967094a48a415ba729995643fcca1c74b7b
kazumi <kazumi121589@yahoo.co.jp>
2022-05-06 23:49:50 +0900

    Created MemoList and MemoDetais classes.

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,16 +1,20 @@
 const fs = require('fs')
+const { Select } = require('enquirer')
 process.stdin.setEncoding('utf8')
 
-class NewMemo {
-  readMemo () {
-    const jsonMemo = fs.readFileSync('./memo_list.json', 'utf8')
-    return JSON.parse(jsonMemo)
-  }
+const readJsonFile = () => {
+  const jsonFile = fs.readFileSync('./memo_list.json', 'utf8')
+  return JSON.parse(jsonFile)
+}
 
-  createMemo () {
+class NewMemo {
+  createMemo() {
     process.stdin.on('data', async input => {
-      const memoObject = { content: input }
-      const memos = await this.readMemo()
+      const memoObject = {
+        title: input.split('\n')[0].trim(),
+        content: input.trim()
+      }
+      const memos = await readJsonFile()
       memos.push(memoObject)
       const newMemos = JSON.stringify(memos)
       fs.writeFileSync('./memo_list.json', newMemos)
@@ -18,4 +22,41 @@ class NewMemo {
   }
 }
 
-new NewMemo().createMemo()
+class MemoList {
+  readFirstLine() {
+    readJsonFile().forEach(element => console.log(element.title))
+  }
+}
+
+class MemoDetails {
+  showDetails() {
+    const prompt = new Select({
+      message: 'Choose a note you want to see',
+      choices: readJsonFile()
+    })
+
+    prompt.run()
+      .then(answer => {
+        prompt.choices.forEach(obj => {
+          if (obj.title === answer) {
+            console.log(obj.content)
+          }
+        })
+      })
+      .catch(console.error)
+  }
+}
+
+switch (process.argv[2]) {
+  case undefined:
+    new NewMemo().createMemo()
+    break
+  case '-l':
+    new MemoList().readFirstLine()
+    break
+  case '-r':
+    new MemoDetails().showDetails()
+    break
+  case '-d':
+    console.log('MemoRemoval')
+}
